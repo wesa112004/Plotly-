c8d89ea3630bc80c907426a17a651a314838f6ed
Wes Adams <wesadams@Wess-MBP.lan>
2022-05-15 22:13:27 -0700

    Plotly

--- plot.js
+++ plot.js
@@ -1,17 +1,33 @@
-console.log(cityGrowths);
-
-var topFiveCityNames = cityGrowths.map(city => city.City);
-var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
-
-var trace = {
-    x: topFiveCityNames,
-    y: topFiveCityGrowths,
-    type: "bar"
+function init() {
+    data = [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16]
+    }];
+    Plotly.newPlot("plot", data);
 };
-var data = [trace];
-var layout = {
-    title: "Most Rapidly Growing Cities",
-    xaxis: { title: "City" },
-    yaxis: { title: "Population Growth, 2016-2017" }
+
+d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+
+function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
+
+    if (dataset === 'dataset1') {
+        yData = [1, 2, 4, 8, 16];
+    };
+
+    if (dataset === 'dataset2') {
+        yData = [1, 10, 100, 1000, 10000];
+    };
+
+    var trace = {
+        x: [xData],
+        y: [yData],
+    };
+    Plotly.restyle("plot", trace);
 };
-Plotly.newPlot("bar-plot", data, layout);
+
+init();
